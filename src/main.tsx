c3640684a4d89b06c7c76941f83cd12519758415
Randy Cullins <randycullins2018@gmail.com>
2024-11-15 17:36:26 -0700

    started working on navbar by adding it to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { TodoProvider } from "./components/context/TodoContext.tsx";
 import "./index.css";
+import Navbar from "./components/layouts/Navbar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TodoProvider>
+      <Navbar />
       <App />
     </TodoProvider>
   </React.StrictMode>
